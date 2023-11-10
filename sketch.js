69ff896ea6d91162b1d14ee28305c5f54cf881f8
ornotwzs <149188853+ornotwzs@users.noreply.github.com>
2023-11-10 16:20:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,12 @@
+function setup() {
+  createCanvas(1000, 1000);
+    background(70,30,90);
+}
 
+function draw() {
+  fill ('rgb(105,32,105)')
+  stroke ('white')
+  strokeWeight (1)
+  if (mouseIsPressed)
+  rect (mouseX, mouseY, 20, 30)
+} 
